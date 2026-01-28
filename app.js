767cec466dc3b3ec1a414eb65f297d5d575fd91b
Ubuntu <ubuntu@ip-10-0-4-1.ec2.internal>
2026-01-28 05:21:25 +0000

    initial nodejs-app

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from End-to-End DevOps Project 🚀');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
+
